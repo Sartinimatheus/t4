dfdd68aad31076c3162a75087d10faff9bd4cb1f
Sartinimatheus <matheussartini214@gmail.com>
2025-08-15 18:38:41 -0300

    Add files via upload

---
+++ BACK-END_T4/atividade6.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial- scale=1.0">
+    <title>Array Associativo</title>
+</head>
+
+<body>
+    <h2>Lista de Pessoas:</h2>
+
+    <?php
+$pessoas = [
+"Ana" => 25,
+"Carlos" => 30,
+
+"Pedro" => 40
+];
+
+foreach ($pessoas as $nome_autor => %$idade	) {
+echo "Nome: " . 	 . ", Idade: " . 	 . "<br>";
+}
+?>
+</body>
+
+</html>
